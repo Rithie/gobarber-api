babf801ad956f4f2535fb03b6e157f4c6488dc8a
Euler Alvarenga <euler.sistemas@gmail.com>
2020-03-01 01:11:59 -0300

    Adiciona suporte à envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: "2525",
+  secure: false,
+  auth: {
+    user: "c9ab79e570104a",
+    pass: "2a265d8a3043fa"
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>"
+  }
+};
