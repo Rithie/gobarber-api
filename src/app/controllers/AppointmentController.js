4fbe500eb66ae200082e56c29795c732acbef882
Euler Alvarenga <euler.sistemas@gmail.com>
2020-04-05 15:25:26 -0300

    FEAT: adiciona fila para processar email e outras tarefas no redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,13 +2,15 @@ import * as Yup from "yup";
 import { startOfHour, parseISO, isBefore, format, subHours } from "date-fns";
 import pt from "date-fns/locale/pt";
 import { Op } from "sequelize";
-import Mail from "../../lib/mail";
 
 import Appointment from "../models/Appointment";
 import User from "../models/User";
 import File from "../models/File";
 
 import Notification from "../schemas/notification";
+
+import CancellationMail from "../jobs/CancellationMail";
+import Queue from "../../lib/Queue";
 
 class AppointmentController {
   async index(req, res) {
@@ -158,18 +160,7 @@ class AppointmentController {
     appointment.cancelled_at = new Date();
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: "Agendamento cancelado",
-      template: "cancellation",
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMM', às ' H:mm'h'", {
-          locale: pt
-        })
-      }
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
