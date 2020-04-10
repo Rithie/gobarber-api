4db1bf9a60f6b8a786f7d291b6d283a4d2b31e1b
Euler Alvarenga <euler.sistemas@gmail.com>
2020-04-10 15:46:39 -0300

    retorna dados do avatar na rota de login e adiciona cors

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,12 +1,22 @@
 import jwt from "jsonwebtoken";
 
 import User from "../models/User";
+import File from "../models/File";
 
 class SessionControler {
   async store(req, res) {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: "avatar",
+          attributes: ["id", "path", "url"]
+        }
+      ]
+    });
 
     if (!user) {
       return res.status(401).json({
@@ -20,13 +30,14 @@ class SessionControler {
       });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar } = user;
 
     return res.json({
       user: {
         id,
         name,
-        email
+        email,
+        avatar
       },
       token: jwt.sign({ id }, "5f61738afb2b32f0ef4e2e58d00a7789", {
         expiresIn: "7d"
